83e308358f3e441cac117df0b440177fe216ab25
Jed Rembold <jedediah.r@gmail.com>
2017-06-14 16:37:01 -0700

    Streamlines code and adds message formatter

--- client.py
+++ client.py
@@ -6,39 +6,41 @@
 #
 # Creation Date: 13-06-2017
 #
-# Last Modified: Tue 13 Jun 2017 04:25:13 PM PDT
+# Last Modified: Wed 14 Jun 2017 04:36:29 PM PDT
 #
 # Created by: Jed Rembold
 #
 #===================================================
 
-import socket
+import socket, struct
 
-def strToBytes( string ):
-    return str.encode(string, 'UTF-8')
-
-def bytesToStr( byte ):
-    return str( byte, 'UTF-8')
-
+def createMessage( msg, needs_reply=False ):
+    ba = bytearray()
+    # Message Initialize
+    ba.extend(b'!!')
+    # Needs reply?
+    if needs_reply:
+        ba.extend(b'01')
+    else:
+        ba.extend(b'00')
+    # 4 byte message
+    if len(msg)>4:
+        raise RuntimeError('Msg too long!')
+    ba.extend(bytes(msg, 'UTF-8'))
+    # End Message
+    ba.extend(b'@@')
+    return ba
 
 message = ''
 while message != 'q':
     sock = socket.create_connection(('localhost',10000))
-
-    message = input('Enter your message:')
+    message = input('Enter your 4 character message: ')
 
     try:
         print('Sending {}'.format(message))
+        msg = createMessage( message )
 
-        sock.sendall(strToBytes(message))
-
-        amount_received = 0
-        amount_expected = len(message)
-
-        while amount_received < amount_expected:
-            data = sock.recv(64)
-            amount_received += len(data)
-            print('Received {}'.format(data))
+        sock.sendall(msg)
 
     finally:
         print('Closing socket')
