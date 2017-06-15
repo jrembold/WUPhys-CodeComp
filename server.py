615b2eecde4b6a581fd04fe266027e450957489f
Jed Rembold <jedediah.r@gmail.com>
2017-06-14 17:35:16 -0700

    Splits away shared socket code and creates responses

--- server.py
+++ server.py
@@ -6,13 +6,14 @@
 #
 # Creation Date: 13-06-2017
 #
-# Last Modified: Wed 14 Jun 2017 04:37:10 PM PDT
+# Last Modified: Wed 14 Jun 2017 05:34:30 PM PDT
 #
 # Created by: Jed Rembold
 #
 #===================================================
 
-import socket, select
+import socket, select, random
+import socket_cmds as scmds
 
 CONNECTION_LIST = []
 PORT = 10000
@@ -26,54 +27,32 @@ def bindAndListen( sock, host, port ):
     sock.listen(1)
     CONNECTION_LIST.append(sock)
 
-def receiveMessage( socket_conn ):
-    '''Reads in the formatted bytearray message'''
+if __name__ == '__main__':
 
-    buf = bytearray()
-    inc_bytes = socket_conn.recv(2)
-    # Check is new message starting
-    if inc_bytes == b'!!':
-        print('Incoming Message!')
-        buf.extend(inc_bytes)
+    # Create the Socket
+    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        # Get reply status
-        buf.extend(socket_conn.recv(2))
+    # Start listening
+    bindAndListen( server_sock, 'localhost', 10000 )
 
-        # Read in 4 byte message
-        buf.extend(socket_conn.recv(4))
+    while True:
+        # Wait for a connection
+        read_socks, write_socks, error_socks = select.select(CONNECTION_LIST, [], [])
 
-        # Check to ensure message ends with terminator
-        last_bytes = socket_conn.recv(2)
-        if last_bytes != b'@@':
-            raise RuntimeError('Error in send message')
-        else:
-            buf.extend(last_bytes)
-    return buf
+        for sock in read_socks:
+            # A New connection
+            if sock == server_sock:
+                sockfd, addr = server_sock.accept()
+                CONNECTION_LIST.append(sockfd)
+                print('Client ({}, {}) connected'.format(addr[0], addr[1]))
 
-
-
-# Create the Socket
-server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Start listening
-bindAndListen( server_sock, 'localhost', 10000 )
-
-while True:
-    # Wait for a connection
-    read_sockets, write_sockets, error_sockets = select.select(CONNECTION_LIST, [], [])
-
-    for sock in read_sockets:
-        # A New connection
-        if sock == server_sock:
-            sockfd, addr = server_sock.accept()
-            CONNECTION_LIST.append(sockfd)
-            print('Client ({}, {}) connected'.format(addr[0], addr[1]))
-
-        #Incoming client message
-        else:
-            msg = receiveMessage( sock )
-            if msg != b'':
-                print(msg)
+            #Incoming client message
+            else:
+                buf, reply, msg = scmds.receiveMessage( sock )
+                if buf != b'':
+                    print(buf)
+                if msg == 'aaaa':
+                    scmds.sendReply(sock, str(random.randint(1,1000)).zfill(4))
 
 
-server_sock.close()
+    server_sock.close()
