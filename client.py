54c818103504a7006e2b907e213b83d9082cd368
Jed Rembold <jedediah.r@gmail.com>
2017-06-14 17:35:38 -0700

    Splits off shared socket code and implements responses

--- client.py
+++ client.py
@@ -6,56 +6,43 @@
 #
 # Creation Date: 13-06-2017
 #
-# Last Modified: Wed 14 Jun 2017 04:47:15 PM PDT
+# Last Modified: Wed 14 Jun 2017 05:35:22 PM PDT
 #
 # Created by: Jed Rembold
 #
 #===================================================
 
 import socket, struct
+import socket_cmds as scmds
 
 HOST = 'localhost'
 PORT = 10000
-CMDS = {}
-CMDS['checkin'] = 'aaaa'
-
-def createMessage( msg, needs_reply=False ):
-    ba = bytearray()
-    # Message Initialize
-    ba.extend(b'!!')
-    # Needs reply?
-    if needs_reply:
-        ba.extend(b'01')
-    else:
-        ba.extend(b'00')
-    # 4 byte message
-    if len(msg)>4:
-        raise RuntimeError('Msg too long!')
-    ba.extend(bytes(msg, 'UTF-8'))
-    # End Message
-    ba.extend(b'@@')
-    return ba
 
 def checkin(sock):
-    msg = createMessage( CMDS['checkin'], True )
+    msg = scmds.createMessage( scmds.CMDS['checkin'], True )
     sock.sendall(msg)
+    buf, reply, msg = scmds.receiveMessage( sock )
+    print(msg)
 
 if __name__ == '__main__':
 
-    message = ''
-    while message != 'q':
+    sock = socket.create_connection((HOST,PORT))
+    checkin(sock)
+
+    while True:
         sock = socket.create_connection((HOST,PORT))
-
-        checkin(sock)
-
         message = input('Enter your 4 character message: ')
+        if message == 'q':
+            break
 
         try:
             print('Sending {}'.format(message))
-            msg = createMessage( message )
+            msg = scmds.createMessage( message )
 
             sock.sendall(msg)
-
-        finally:
-            print('Closing socket')
+        except:
+            print('Error occured. Closing socket')
             sock.close()
+
+    print('Closing socket')
+    sock.close()
