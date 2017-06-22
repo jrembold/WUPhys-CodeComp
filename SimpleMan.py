ceb33132377cbb010f010a6ef6cbcc44bdd899e6
Jed Rembold <jedediah.r@gmail.com>
2017-06-22 15:08:06 -0700

    Adds some basic sample bots

--- SimpleMan.py
+++ SimpleMan.py
@@ -6,23 +6,29 @@
 #
 # Creation Date: 20-06-2017
 #
-# Last Modified: Tue 20 Jun 2017 06:43:37 PM PDT
+# Last Modified: Thu 22 Jun 2017 03:03:08 PM PDT
 #
 # Created by: Jed Rembold
 #
 #===================================================
 
 import library as lib
-import time, random
 
-bot = lib.CBot()
+def calcMove( bot ):
+    #If anything non-zero in front to me, go forwards!
+    if any(bot.vision[1:]):
+        return 'forward'
+    #Otherwise, turn clockwise
+    else:
+        return 'rotCW'
+
+
+
+bot = lib.CBot('Simple Simon')
 
 while bot.active:
     bot.getMapState()
 
     if bot.active:
-        move = random.choice(['forward', 'rotCW', 'rotCCW', 'forward'])
+        move = calcMove(bot)
         bot.sendMessage( move )
-        # time.sleep(0.15)
-
-        # print('I see {}'.format(bot.vision))
