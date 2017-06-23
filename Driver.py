83e304d86cbf59042b448f4f3dfb9815b244463e
Jed Rembold <jedediah.r@gmail.com>
2017-06-22 18:16:15 -0700

    Adds controllable bot

---
+++ Driver.py
@@ -0,0 +1,51 @@
+#===================================================
+#
+# File Name: Driver.py
+# 
+# Purpose: A custom bot to allow for user interaction
+#
+# Creation Date: 20-06-2017
+#
+# Last Modified: Thu 22 Jun 2017 06:08:24 PM PDT
+#
+# Created by: Jed Rembold
+#
+#===================================================
+
+import library as lib
+
+def getMove(sel ):
+    if sel == '1':
+        return 'forward'
+    if sel == '2':
+        return 'rotCW'
+    if sel == '3':
+        return 'rotCCW'
+    if sel == '4':
+        return 'spear'
+    else:
+        return 'leave'
+
+def getSelection():
+    menu = {}
+    menu['1'] = ' - Move Forward'
+    menu['2'] = ' - Rotate CW'
+    menu['3'] = ' - Rotate CCW'
+    menu['4'] = ' - Throw Spear'
+    menu['q'] = ' - Suicide'
+
+    options=menu.keys()
+    for entry in options:
+        print(entry, menu[entry])
+    selection = input('Please select: ')
+    return selection
+
+
+bot = lib.CBot('Driver')
+
+while bot.active:
+    bot.getMapState()
+
+    if bot.active:
+        move = getMove(getSelection())
+        bot.sendMessage( move )
