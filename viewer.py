d80bf8a242796e8d237b8e6d463427823f0023e2
Jed Rembold <jedediah.r@gmail.com>
2017-06-26 00:21:21 -0700

    Adds full arrow tracking and drawing

--- viewer.py
+++ viewer.py
@@ -6,7 +6,7 @@
 #
 # Creation Date: 25-06-2017
 #
-# Last Modified: Sun 25 Jun 2017 11:38:49 PM PDT
+# Last Modified: Mon 26 Jun 2017 12:21:09 AM PDT
 #
 # Created by: Jed Rembold
 #
@@ -26,9 +26,21 @@ def getPlayerDirSym(player):
     if player['face'] == 3: return '<'
     return 'o'
 
+def getSpearDirSym(spear):
+    if spear[2] == 0: return '$↑$'
+    if spear[2] == 1: return '$→$'
+    if spear[2] == 2: return '$↓$'
+    if spear[2] == 3: return '$←$'
+
+def getSpearColor(spear):
+    if spear[3]: return 'red'
+    return 'gray'
+
 width, height = mapstate['Map'].shape
 fig = plt.figure()
 ax = fig.add_axes((0.05,0.05,0.9,0.9), aspect='equal', xlim=(0,width), ylim=(0,height))
+ax.xaxis.set_visible(False)
+ax.yaxis.set_visible(False)
 
 
 numrounds = max([x for x in mapstate.keys() if isinstance(x,int)])
@@ -39,7 +51,7 @@ for rnd in range(numrounds+1):
         player = mapstate[rnd]['players'][p]
         ax.scatter(player['x'], player['y'], marker=getPlayerDirSym(player), label=player['name'])
     for s in mapstate[rnd]['spears']:
-        ax.scatter(s[0], s[1], marker='2', color='gray')
+        ax.scatter(s[0], s[1], marker=getSpearDirSym(s), color=getSpearColor(s))
     ax.legend(loc='center left', bbox_to_anchor=(1,0.5))
     plt.pause(0.05)
 
