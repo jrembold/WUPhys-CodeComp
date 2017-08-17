88dae7ca42972dab5cfe020756048b7f7ef72455
Jed Rembold <jedediah.r@gmail.com>
2017-08-17 13:32:00 -0700

    Cleans up code and logs new ping dict

--- DumbPinger.py
+++ DumbPinger.py
@@ -6,7 +6,7 @@
 #
 # Creation Date: 20-06-2017
 #
-# Last Modified: Tue 18 Jul 2017 07:08:40 PM PDT
+# Last Modified: Thu 17 Aug 2017 01:31:44 PM PDT
 #
 # Created by: Jed Rembold
 #
@@ -32,59 +32,11 @@ logging.basicConfig(
         filemode = 'w',
         )
 
-def getObjDir( obj ):
-    d = round(obj % 1, 1)
-    if d == 0.0: return 'North'
-    if d == 0.1: return 'East'
-    if d == 0.2: return 'South'
-    if d == 0.3: return 'West'
-    return None
-
-def opposite( direction ):
-    if direction=='North': return 'South'
-    if direction=='South': return 'North'
-    if direction=='East': return 'West'
-    if direction=='West': return 'East'
-
 def calcMove( bot):
-    global tcount
-    #v = np.array(bot.vision[1:])
-
-    #Get facing direction
-    #botdir = getObjDir(bot.vision[0])
-
     return 'ping'
-
-    # #Do I see a bot?
-    # if any(v>10):
-        # #Empty space in front?
-        # if v[0] == 0:
-            # #Have spear onhand?
-            # if bot.spearcount>0:
-                # #Already spear enroute?
-                # if any([opposite(getObjDir(i))==botdir for i in v if floor(i)==2]):
-                    # return 'spear'
-                # elif any([floor(i)==2 for i in v]):
-                    # return 'rotCW'
-                # else:
-                    # return 'spear'
-    # if any(v==3) and not any(v>10):
-        # tcount = 0
-        # return 'forward'
-    # #if stationary too long, move
-    # if tcount > 3 and len(v)>0:
-        # tcount = 0
-        # return 'forward'
-    # else:
-        # #Otherwise, turn clockwise
-        # tcount += 1
-        # return 'rotCW'
-
 
 
 bot = lib.CBot('Anita Pinger')
-tcount = 0
-roundnum = 0
 
 while bot.active:
     bot.getMapState()
@@ -95,5 +47,6 @@ while bot.active:
         move = calcMove(bot)
         logging.info('[Round: {:^2}]: Sent move: {}'.format(roundnum,move))
         bot.sendMessage( move )
+        logging.debug(bot.lastping)
 
     roundnum += 1
