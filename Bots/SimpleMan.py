3f7ac0e48459a20dd81d58f4af778db92b3f8144
Jed Rembold <jedediah.r@gmail.com>
2017-08-17 17:33:48 -0700

    Adds better documentation and cleans up

--- Bots/SimpleMan.py
+++ Bots/SimpleMan.py
@@ -2,34 +2,40 @@
 #
 # File Name: Simpleman.py
 # 
-# Purpose: A simple bot for stabbing things
-#
-# Creation Date: 20-06-2017
-#
-# Last Modified: Fri 23 Jun 2017 03:04:13 PM PDT
+# Purpose: A simple bot for charging at things
 #
 # Created by: Jed Rembold
 #
 #===================================================
-import sys
-sys.path.append('./')
-import library as lib
 
+# -- Importing Stuff --
+import sys              #Grabbing sys for the below command
+sys.path.append('./')   #This lets us keep the bots in a sep folder
+import library as lib   #Importing the main library
+
+
+'''This time I broke the bot decision logic out into its
+own function. This makes it a bit easier for me to keep
+track of everything, but is totally not necessary!'''
+
+# This function decides what the bot will do
 def calcMove( bot ):
-    #If anything non-zero in front to me, go forwards!
-    if any(bot.vision[1:]):
+    #Anything in my vision that is non-zero is either
+    #an enemy or a spear. So if I see one, go forwards!
+    if any(bot.vision[1:]):     #any means if any of the values I see are >0, then go
         return 'forward'
-    #Otherwise, turn clockwise
+    #If I see only zeros (air) then I'll turn looking for something else
     else:
         return 'rotCW'
 
 
+# -- Initializing the Bot --
+bot = lib.CBot('Simple Simon')  #Giving it a cool (not really) name!
 
-bot = lib.CBot('Simple Simon')
+# -- Main Loop --
+while bot.active:               #while alive,
+    bot.getMapState()           #get the current map state
 
-while bot.active:
-    bot.getMapState()
-
-    if bot.active:
-        move = calcMove(bot)
-        bot.sendMessage( move )
+    if bot.active:              #if still alive
+        move = calcMove(bot)    #calculate our best move
+        bot.sendMessage( move ) #send best move to server
