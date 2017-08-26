93242f45a77410dcb78a1695663a3d3db882c712
Jed Rembold <jedediah.r@gmail.com>
2017-08-25 17:23:09 -0700

    Tweaks to finetune ranking algorithms

--- leaderboards.py
+++ leaderboards.py
@@ -11,21 +11,66 @@ import random
 import numpy as np
 import json
 
-records = {'Amy the Amazon': 1000}
+initval = 1000
 
-winners = []
-for i in range(10):
+def diffalg(diff):
+    return max(1,0.1*diff+50)
+
+for i in range(200):
+    # Read in records
+    if os.path.isfile('scores.json'):
+        with open('scores.json', 'r') as f:
+            ratings = json.load(f)
+    else:
+        ratings = {}
+
     # Get random list of bots
     bots = np.random.choice(
             os.listdir('Bots'),
             replace=False,
             size=np.random.randint(2,5)
             )
+    print(bots)
 
-    winners.append(server.main(bots, 20, 10, False, 1, replaysave=False))
+    # If a new bot, initialize its rating
+    for bot in bots:
+        if bot not in ratings.keys():
+            ratings[bot] = [initval]
 
-print(winners)
+    # Get all bots ratings
+    botRatings = [ratings[bot][-1] for bot in bots]
 
-with open('scores.json', 'w') as f:
-    json.dump(records, f)
-    
+    # Average Rating
+    avgRating = np.mean(botRatings)
+
+    # Subtract buyins from ratings
+    buyinpool = 0
+    for bot in bots:
+        # Get rating difference
+        diff = ratings[bot][-1] - avgRating
+        # Calculate Buyin
+        buyin = diffalg(diff)
+        buyinpool += buyin
+        ratings[bot].append(ratings[bot][-1] - buyin)
+
+    victornum = server.main(bots, 20, 10, False, 1, replaysave=False)
+    if victornum != '':
+        victoridx = int(victornum) - 51
+        ratings[bots[victoridx]].append(float(ratings[bots[victoridx]][-1]) + buyinpool)
+    else:
+        for bot in bots:
+            ratings[bot].append(ratings[bot][-1] + buyinpool/len(bots))
+
+    with open('scores.json', 'w') as f:
+        json.dump(ratings, f)
+   
+import matplotlib
+import matplotlib.pyplot as plt
+
+for key in ratings:
+    plt.plot(ratings[key], label=key)
+plt.legend()
+plt.xlabel('Games')
+plt.ylabel('Rating')
+plt.show()
+
